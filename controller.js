44ec24820edae0a0ba018b51bb19d75b03d1c411
anugrahsyawal <nugrahsyawal@gmail.com>
2023-07-03 22:27:57 +0800

    berhasil menampilkan semua data tabel am4

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require("./koneksi");
 exports.index = function (req, res) {
   response.ok("Aplikasi REST API ku berjalan!", res);
 };
+
+// menampilkan data processor socket am4
+exports.tampilsemuaam4 = function (req, res) {
+  connection.query("SELECT * FROM am4", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
