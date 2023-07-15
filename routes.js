6f07cf89a195a1e07657671faf6f983b52678e0b
anugrahsyawal <nugrahsyawal@gmail.com>
2023-07-15 17:09:29 +0800

    penyesuaian routes.js sesuai dengan perubahan terakhir

--- routes.js
+++ routes.js
@@ -6,7 +6,22 @@ module.exports = function (app) {
 
   // app.route("/").get(jsonku.index);
 
-  app.route("/get-am4").get(jsonku.getAllam4);
+  app.route("/get-amd").get(jsonku.getAllDataAMD);
+  app.route("/get-amd-am4").get(jsonku.getDataAM4);
+  app.route("/get-amd-am3").get(jsonku.getDataAM3);
+  app.route("/get-amd-am2").get(jsonku.getDataAM2);
+  app.route("/get-amd-fm2").get(jsonku.getDataFM2);
+
+  app.route("/get-intel").get(jsonku.getAllDataIntel);
+  app.route("/get-intel-lga1150").get(jsonku.getDataLGA1150);
+  app.route("/get-intel-lga1151").get(jsonku.getDataLGA1151);
+  app.route("/get-intel-lga1155").get(jsonku.getDataLGA1155);
+  app.route("/get-intel-lga1156").get(jsonku.getDataLGA1156);
+  app.route("/get-intel-lga1356").get(jsonku.getDataLGA1356);
+  app.route("/get-intel-lga1366").get(jsonku.getDataLGA1366);
+  app.route("/get-intel-lga2011").get(jsonku.getDataLGA2011);
+  app.route("/get-intel-lga2011").get(jsonku.getDataLGA2011);
+  app.route("/get-intel-lga2011_3").get(jsonku.getDataLGA2011_3);
 
   app.route("/get-am4/:seri").get(jsonku.getSeriam4);
 
