f2dc16d785dd78b1244f0bc30ac0a680892e3a57
anugrahsyawal <nugrahsyawal@gmail.com>
2023-07-16 14:05:37 +0800

    kali ini udah hampir fix endpointnya

--- controller.js
+++ controller.js
@@ -77,6 +77,112 @@ exports.getDataFM2 = function (req, res) {
   );
 };
 
+// menampilkan cpu amd family Ryzen
+exports.getDataRyzen = function (req, res) {
+  connection.query(
+    "SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name FROM amd WHERE name LIKE '%Ryzen%' ORDER BY name DESC;",
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family Phenom
+exports.getDataPhenom = function (req, res) {
+  connection.query(
+    "SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name FROM amd WHERE name LIKE '%Phenom%' ORDER BY name DESC;",
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family Athlon
+exports.getDataAthlon = function (req, res) {
+  connection.query(
+    `SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name
+    FROM amd 
+    WHERE name LIKE '%Athlon%' AND name NOT LIKE '%II%' 
+    ORDER BY name DESC;`,
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family Athlon II
+exports.getDataAthlon_II = function (req, res) {
+  connection.query(
+    "SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name FROM amd WHERE name LIKE '%Athlon II%' ORDER BY name DESC;",
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family Sempron
+exports.getDataSempron = function (req, res) {
+  connection.query(
+    "SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name FROM amd WHERE name LIKE '%Sempron%' ORDER BY name DESC;",
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family Opteron
+exports.getDataOpteron = function (req, res) {
+  connection.query(
+    `SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name
+    FROM amd WHERE name LIKE '%Opteron%' ORDER BY name DESC;`,
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family A series
+exports.getDataA = function (req, res) {
+  connection.query(
+    `SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name
+    FROM amd
+    WHERE name LIKE '%A%' AND name NOT LIKE '%l%'
+    ORDER BY name DESC;
+    `,
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
 // menampilkan semua data cpu intel
 exports.getAllDataIntel = function (req, res) {
   connection.query(
@@ -217,11 +323,14 @@ exports.getDataLGA775 = function (req, res) {
   );
 };
 
-// menampilkan data processor socket am4 tertentu
-exports.getSeriam4 = function (req, res) {
-  let seri = req.params.seri;
+// menampilkan cpu amd family Xeon series
+exports.getDataXeon = function (req, res) {
   connection.query(
-    `SELECT * FROM am4 WHERE name LIKE '%Ryzen ${seri}%';`,
+    `SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name
+    FROM intel
+    WHERE name LIKE '%Xeon%'
+    ORDER BY name DESC;
+    `,
     function (error, rows, fields) {
       if (error) {
         console.log(error);
@@ -232,8 +341,80 @@ exports.getSeriam4 = function (req, res) {
   );
 };
 
-// menambahkan data tabel am4
-exports.tambahdataam4 = function (req, res) {
+// menampilkan cpu amd family Pentium series
+exports.getDataPentium = function (req, res) {
+  connection.query(
+    `SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name
+    FROM intel
+    WHERE name LIKE '%Pentium%'
+    ORDER BY name DESC;
+    `,
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family Celeron series
+exports.getDataCeleron = function (req, res) {
+  connection.query(
+    `SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name
+    FROM intel
+    WHERE name LIKE '%Celeron%'
+    ORDER BY name DESC;
+    `,
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family Core 2 series
+exports.getDataCore_2 = function (req, res) {
+  connection.query(
+    `SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name
+    FROM intel
+    WHERE name LIKE '%Core 2%'
+    ORDER BY name DESC;
+    `,
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menampilkan cpu amd family Core i
+exports.getDataCore_i = function (req, res) {
+  connection.query(
+    `SELECT name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name
+    FROM intel
+    WHERE name LIKE '%Core i%'
+    ORDER BY name DESC;
+    `,
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+// menambahkan data tabel amd
+exports.insertDataAMD = function (req, res) {
   var name = req.body.name;
   var performance = req.body.performance;
   var price = req.body.price;
@@ -246,10 +427,11 @@ exports.tambahdataam4 = function (req, res) {
   var technology = req.body.technology;
   var ddr_support = req.body.ddr_support;
   var code_name = req.body.code_name;
+  var socket = req.body.socket;
 
   connection.query(
-    `INSERT INTO am4 (name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name)
-    VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`,
+    `INSERT INTO amd (name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name, socket)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`,
     [
       name,
       performance,
@@ -263,6 +445,7 @@ exports.tambahdataam4 = function (req, res) {
       technology,
       ddr_support,
       code_name,
+      socket,
     ],
     function (error, rows, fields) {
       if (error) {
@@ -275,8 +458,8 @@ exports.tambahdataam4 = function (req, res) {
 };
 
 // mengubah data berdasarkan id
-exports.ubahdataam4 = function (req, res) {
-  var id_am4 = req.body.id_am4;
+exports.updateDataAMD = function (req, res) {
+  var id_amd = req.body.id_amd;
   var name = req.body.name;
   var performance = req.body.performance;
   var price = req.body.price;
@@ -289,9 +472,10 @@ exports.ubahdataam4 = function (req, res) {
   var technology = req.body.technology;
   var ddr_support = req.body.ddr_support;
   var code_name = req.body.code_name;
+  var socket = req.body.socket;
 
   connection.query(
-    `UPDATE am4 SET name=?, performance=?, price=?, cores=?, frequency=?, cache_memory=?, tdp=?, video=?, graphic_speed=?, technology=?, ddr_support=?, code_name=? WHERE id_am4=?`,
+    `UPDATE amd SET name=?, performance=?, price=?, cores=?, frequency=?, cache_memory=?, tdp=?, video=?, graphic_speed=?, technology=?, ddr_support=?, code_name=?, socket=? WHERE id_amd=?`,
     [
       name,
       performance,
@@ -305,7 +489,8 @@ exports.ubahdataam4 = function (req, res) {
       technology,
       ddr_support,
       code_name,
-      id_am4,
+      socket,
+      id_amd,
     ],
     function (error, rows, fields) {
       if (error) {
@@ -317,11 +502,115 @@ exports.ubahdataam4 = function (req, res) {
   );
 };
 
-// menghapus data am4
-exports.hapusdataam4 = function (req, res) {
-  var id_am4 = req.body.id_am4;
+// menghapus data amd
+exports.deleteDataAMD = function (req, res) {
+  var id_amd = req.body.id_amd;
   connection.query(
-    `DELETE FROM am4 WHERE id_am4 = ${id_am4}`,
+    `DELETE FROM amd WHERE id_amd = ${id_amd}`,
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil Menghapus Data", res);
+      }
+    }
+  );
+};
+
+// menambahkan data tabel intel
+exports.insertDataIntel = function (req, res) {
+  var name = req.body.name;
+  var performance = req.body.performance;
+  var price = req.body.price;
+  var cores = req.body.cores;
+  var frequency = req.body.frequency;
+  var cache_memory = req.body.cache_memory;
+  var tdp = req.body.tdp;
+  var video = req.body.video;
+  var graphic_speed = req.body.graphic_speed;
+  var technology = req.body.technology;
+  var ddr_support = req.body.ddr_support;
+  var code_name = req.body.code_name;
+  var socket = req.body.socket;
+
+  connection.query(
+    `INSERT INTO intel (name, performance, price, cores, frequency, cache_memory, tdp, video, graphic_speed, technology, ddr_support, code_name, socket)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+    [
+      name,
+      performance,
+      price,
+      cores,
+      frequency,
+      cache_memory,
+      tdp,
+      video,
+      graphic_speed,
+      technology,
+      ddr_support,
+      code_name,
+      socket,
+    ],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil menambahkan Data", res);
+      }
+    }
+  );
+};
+
+// mengubah data berdasarkan id
+exports.updateDataIntel = function (req, res) {
+  var id_intel = req.body.id_intel;
+  var name = req.body.name;
+  var performance = req.body.performance;
+  var price = req.body.price;
+  var cores = req.body.cores;
+  var frequency = req.body.frequency;
+  var cache_memory = req.body.cache_memory;
+  var tdp = req.body.tdp;
+  var video = req.body.video;
+  var graphic_speed = req.body.graphic_speed;
+  var technology = req.body.technology;
+  var ddr_support = req.body.ddr_support;
+  var code_name = req.body.code_name;
+  var socket = req.body.socket;
+
+  connection.query(
+    `UPDATE intel SET name=?, performance=?, price=?, cores=?, frequency=?, cache_memory=?, tdp=?, video=?, graphic_speed=?, technology=?, ddr_support=?, code_name=?, socket=? WHERE id_intel=?`,
+    [
+      name,
+      performance,
+      price,
+      cores,
+      frequency,
+      cache_memory,
+      tdp,
+      video,
+      graphic_speed,
+      technology,
+      ddr_support,
+      code_name,
+      socket,
+      id_intel,
+    ],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil Mengubah Data", res);
+      }
+    }
+  );
+};
+
+// menghapus data intel
+exports.deleteDataIntel = function (req, res) {
+  var id_intel = req.body.id_intel;
+  connection.query(
+    `DELETE FROM intel WHERE id_intel = ${id_intel}`,
     function (error, rows, fields) {
       if (error) {
         console.log(error);
--- routes.js
+++ routes.js
@@ -11,6 +11,13 @@ module.exports = function (app) {
   app.route("/get-amd-am3").get(jsonku.getDataAM3);
   app.route("/get-amd-am2").get(jsonku.getDataAM2);
   app.route("/get-amd-fm2").get(jsonku.getDataFM2);
+  app.route("/get-amd-ryzen").get(jsonku.getDataRyzen);
+  app.route("/get-amd-phenom").get(jsonku.getDataPhenom);
+  app.route("/get-amd-athlon").get(jsonku.getDataAthlon);
+  app.route("/get-amd-athlon2").get(jsonku.getDataAthlon_II);
+  app.route("/get-amd-sempron").get(jsonku.getDataSempron);
+  app.route("/get-amd-a").get(jsonku.getDataA);
+  app.route("/get-amd-operon").get(jsonku.getDataOpteron);
 
   app.route("/get-intel").get(jsonku.getAllDataIntel);
   app.route("/get-intel-lga1150").get(jsonku.getDataLGA1150);
@@ -22,12 +29,17 @@ module.exports = function (app) {
   app.route("/get-intel-lga2011").get(jsonku.getDataLGA2011);
   app.route("/get-intel-lga2011").get(jsonku.getDataLGA2011);
   app.route("/get-intel-lga2011_3").get(jsonku.getDataLGA2011_3);
+  app.route("/get-intel-xeon").get(jsonku.getDataXeon);
+  app.route("/get-intel-pentium").get(jsonku.getDataPentium);
+  app.route("/get-intel-celeron").get(jsonku.getDataCeleron);
+  app.route("/get-intel-core-2").get(jsonku.getDataCore_2);
+  app.route("/get-intel-core-i").get(jsonku.getDataCore_i);
 
-  app.route("/get-am4/:seri").get(jsonku.getSeriam4);
+  app.route("/insert-amd").post(verifikasi(1), jsonku.insertDataAMD);
+  app.route("/update-amd").put(verifikasi(1), jsonku.updateDataAMD);
+  app.route("/delete-amd").delete(verifikasi(1), jsonku.deleteDataAMD);
 
-  app.route("/tambah").post(verifikasi(1), jsonku.tambahdataam4);
-
-  app.route("/ubah").put(verifikasi(1), jsonku.ubahdataam4);
-
-  app.route("/hapus").delete(verifikasi(1), jsonku.hapusdataam4);
+  app.route("/insert-intel").post(verifikasi(1), jsonku.insertDataIntel);
+  app.route("/update-intel").put(verifikasi(1), jsonku.updateDataIntel);
+  app.route("/delete-intel").delete(verifikasi(1), jsonku.deleteDataIntel);
 };
